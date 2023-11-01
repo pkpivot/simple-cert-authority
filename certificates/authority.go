bc636cc1499212a8a6bc12785f8cb69d9a840c46
Paul Kelly <kellypa@vmware.com>
2023-11-01 16:41:45 +0000

    Add certificate signing function

--- certificates/authority.go
+++ certificates/authority.go
@@ -134,21 +134,58 @@ func CreateSigningSet(filename string, host string) {
 }
 
 func CreateSignedCertificate(filename string, host string, signingSet string) {
-	//certName, keyName := CertSetNames(filename)
-	//
-	//x509.
-	//
-	//certTemplate, privateKey, err := CreateTemplateCertificateAndKey(host)
-	//if err != nil {
-	//	log.Fatalf("Could not create cert template and key %v", err)
-	//}
-	//
-	//file, err := os.Create(filename)
-	//if err != nil {
-	//	log.Fatalf("Failed to open key file for writing: %v", err)
-	//}
-	//
-	//err2 := WritePemCertFile(certTemplate, signingCert, signingKey, file)
+
+	// 1. Create all filenames
+	certName, keyName := CertSetNames(filename)
+	signingCertName, signingKeyName := CertSetNames(signingSet)
+
+	// 2. Read the PEM files for signing cert and key
+	signingCertPem, err := os.ReadFile(signingCertName)
+	if err != nil {
+		log.Fatalf("Could not read signing certificate certFile %v", err)
+	}
+
+	signingKeyPem, err := os.ReadFile(signingKeyName)
+	if err != nil {
+		log.Fatalf("Could not read signing key certFile %v", err)
+	}
+
+	// 3. Parse the PEM files to create signing cert and signing key objects
+	signingCert, err := ReadPemCert(signingCertPem)
+	if err != nil {
+		log.Fatalf("Could not parse pem certificate certFile %v", err)
+	}
+
+	signingKey, err := ReadPemKey(signingKeyPem)
+	if err != nil {
+		log.Fatalf("Could not parse pem key certFile %v", err)
+	}
+
+	// 4. Generate the client certificate key pair
+	certTemplate, privateKey, err := CreateTemplateCertificateAndKey(host)
+	if err != nil {
+		log.Fatalf("Could not create cert template and key %v", err)
+	}
+
+	keyFile, err := os.Create(keyName)
+	defer keyFile.Close()
+
+	// 4. Write the private key certFile
+	if err := WritePemPrivateKey(&privateKey, keyFile); err != nil {
+		log.Fatalf("Could open private key certFile %v", err)
+	}
+
+	// 5. Sign the certificate and write it out
+	certFile, err := os.Create(certName)
+	defer certFile.Close()
+
+	if err != nil {
+		log.Fatalf("Failed to open key certFile for writing: %v", err)
+	}
+
+	if err := WritePemCertFile(*certTemplate, *signingCert, signingKey, certFile); err != nil {
+		log.Fatalf("Could not create signed certificate: %v", err)
+	}
 }
 
 func CertSetNames(setName string) (string, string) {
@@ -167,6 +204,7 @@ func main() {
 	signCmd := flag.NewFlagSet("sign", flag.ExitOnError)
 	signedHost := signCmd.String("host", "signed.example.com", "URL of host to created signed certificate for")
 	signedFilename := signCmd.String("filename", "signed-certificate", "Name of the signed ")
+	signingCert := signCmd.String("signer", "signing-cert", "Name of the signing set")
 
 	if len(os.Args) < 2 {
 		fmt.Println("Expected root command")
@@ -179,7 +217,7 @@ func main() {
 		CreateSigningSet(*filename, *host)
 
 	case "sign":
-		rootCmd.Parse(os.Args[2:])
-		CreateSignedCertificate(*signedFilename, *signedHost, "signing-certificate")
+		signCmd.Parse(os.Args[2:])
+		CreateSignedCertificate(*signedFilename, *signedHost, *signingCert)
 	}
 }
